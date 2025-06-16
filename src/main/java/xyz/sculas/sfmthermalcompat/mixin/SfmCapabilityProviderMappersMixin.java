b0181fde4d10704cfc011b8a9ce8baf0dbcf2676
Sculas <contact@sculas.xyz>
2025-06-16 21:54:49 +0200

    fix: don't use registries so the mod can be server-only

--- src/main/java/xyz/sculas/sfmthermalcompat/mixin/SfmCapabilityProviderMappersMixin.java
+++ src/main/java/xyz/sculas/sfmthermalcompat/mixin/SfmCapabilityProviderMappersMixin.java
@@ -2,26 +2,29 @@ package xyz.sculas.sfmthermalcompat.mixin;
 
 import ca.teamdman.sfm.common.capabilityprovidermapper.CapabilityProviderMapper;
 import ca.teamdman.sfm.common.registry.SFMCapabilityProviderMappers;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.sculas.sfmthermalcompat.SfmThermalCompat;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xyz.sculas.sfmthermalcompat.common.mapper.AugmentableBlockEntityCapabilityProviderMapper;
+
+import java.util.Collection;
+import java.util.List;
 
 @Mixin(value = SFMCapabilityProviderMappers.class, remap = false)
 public abstract class SfmCapabilityProviderMappersMixin {
-    @Final
-    @Shadow
-    private static DeferredRegister<CapabilityProviderMapper> REGISTERER;
+    @Unique
+    private static final List<CapabilityProviderMapper> sfmThermalCompat$MAPPERS = List.of(
+            new AugmentableBlockEntityCapabilityProviderMapper()
+    );
 
-    @Inject(method = "register", at = @At("HEAD"), remap = false)
-    private static void register(IEventBus bus, CallbackInfo ci) {
-        SfmThermalCompat.LOGGER.info("Injecting capability provider mappers into SFM...");
-        REGISTERER.register("thermal/augment", AugmentableBlockEntityCapabilityProviderMapper::new);
+    @Inject(method = "discoverCapabilityProvider", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraftforge/registries/IForgeRegistry;getValues()Ljava/util/Collection;"), locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
+    private static void discoverCapabilityProvider(Level level, BlockPos pos, CallbackInfoReturnable<ICapabilityProvider> cir, Collection<CapabilityProviderMapper> mappers) {
+        mappers.addAll(sfmThermalCompat$MAPPERS);
     }
 }
